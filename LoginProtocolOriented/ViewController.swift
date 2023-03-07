1a1a8ead5a1766a7a30150cb1bfbe50a5f851ba1
Merve Dönmez <merve.donmez@ibtech.com.tr>
2023-03-07 19:42:16 +0300

    Initial Commit

---
+++ LoginProtocolOriented/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginProtocolOriented
+//
+//  Created by C02QJ4LQG8WN on 3/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
